5941c62b3bfa47726d2722bc8e86a3cbe84a5078
Faizaan Khan <faizaankhan24@gmail.com>
2022-11-09 21:50:20 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Faizaan Khan on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
