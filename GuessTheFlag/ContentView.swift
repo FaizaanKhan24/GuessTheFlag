5d9606d5049f5f983a5a8c25533d611d1ce861ee
Faizaan Khan <faizaankhan24@gmail.com>
2022-12-06 10:08:38 -0500

    Adding animations

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,9 +12,12 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var score = 0
     @State private var alertMessage = ""
+    @State private var scoreColor: Color = Color.white
     
     @State private var isGameOver = false
     @State var gameCounter = 0
+    
+    @State private var rotationAmount = 0.0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -52,17 +55,24 @@ struct ContentView: View {
                 ForEach(0..<3){ number in
                     Button{
                         flagTapped(number)
+//                        rotationAmount += 360
                     } label: {
                         FlagImage(imageName: countries[number])
-                        
+                            .rotation3DEffect(.degrees( number == correctAnswer ?  rotationAmount : 0), axis: (x:0, y:1, z:0))
+                            .opacity(isShowScore && number != correctAnswer ? 0.25 : 1)
+                            .scaleEffect(isShowScore && number != correctAnswer ? 0.75: 1)
                     }
                 }
                 
                 Spacer()
                 
                 VStack{
-                    Text("Your Score : \(score)")
+                    Text("Your Score : ")
                         .foregroundColor(.white)
+                        .font(.subheadline.bold())
+                    +
+                    Text("\(score)")
+                        .foregroundColor(scoreColor)
                         .font(.subheadline.bold())
                 }
             }
@@ -83,18 +93,29 @@ struct ContentView: View {
     }
     
     func flagTapped(_ number: Int){
+        withAnimation(){
+            rotationAmount += 360
+        }
         if number == correctAnswer{
             scoreTitle = "Correct"
-            score += 1
+            withAnimation(.easeIn(duration: 0.5)){
+                score += 1
+                scoreColor = .green
+            }
             alertMessage = "Awesome! That's the correct answer"
         }
         else{
             scoreTitle = "Wrong"
-            score -= 1
+            withAnimation(.easeOut(duration: 0.5)){
+                score -= 1
+                scoreColor = .red
+            }
             alertMessage = "Wrong! Thats the flag of \(countries[number])"
         }
         
-        isShowScore = true
+        withAnimation(.easeIn(duration: 1)){
+            isShowScore = true
+        }
         gameCounter += 1
         if (gameCounter >= 8){
             isGameOver = true
